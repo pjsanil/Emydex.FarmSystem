1a694d9333b6fa1062e06ed83532feef111afa55
pjsanil <pjsanil@gmail.com>
2021-11-24 12:29:13 +0000

    Add project files.

---
+++ FarmSystem.Test1/EmydexFarmSystem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmSystem.Test1
+{
+    public class EmydexFarmSystem
+    {
+        //TEST 1
+        List<string> animalsList = new List<string>();
+        Cow cw = new Cow();
+        Hen hn = new Hen();
+        Horse hs = new Horse();
+        Sheep sh = new Sheep();
+        public delegate void linktoeventhandler();
+        
+        
+        private string getAnimalType(object animal)
+		{
+            return animal.GetType().Name;
+        }
+        public void Enter(object animal)
+        {
+            string AnimalType = getAnimalType(animal);
+            Global.AnimalTypeList(AnimalType);
+   
+
+
+            Console.WriteLine("{0} has entered the farm", AnimalType);
+        }
+     
+        //TEST 2
+        public void MakeNoise()
+        {
+            //Test 2 : Modify this method to make the animals talk
+            cw.Talk();
+            hn.Talk();
+            hs.Talk();
+            sh.Talk();
+
+            
+        }
+
+        //TEST 3
+        public void MilkAnimals()
+        {
+            cw.ProduceMilk();
+           
+        }
+
+
+        public void eventhandler()
+        {
+			
+			Console.WriteLine("Emydex Farm is now empty");
+        }
+
+      
+
+        //TEST 4
+        public void ReleaseAllAnimals()
+        {
+            Global.ReleaseAnimal();
+           Console.WriteLine("There are still animals in the farm, farm is not free");
+        }
+    }
+}
---
+++ FarmSystem.Test1/Global.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmSystem.Test1
+{
+	public static class Global
+	{
+		
+
+		private static List<string> animals = new List<string>();
+        static string animaltype;
+        public static void AnimalTypeList(string AnimalType)
+        {
+            if (!(animals.Contains(AnimalType)))
+                {
+                animals.Add(AnimalType);
+
+               }
+
+              
+        }
+
+        public static void ReleaseAnimal()
+		{
+            int i = 0;
+            if (animals.Count >0)
+			{
+                while(!(animals.Count==0))
+                {
+                    animaltype = animals[i];
+                    
+                    animals.Remove(animaltype);
+                    Console.WriteLine("{0} has left the farm", animaltype);
+                    
+                }
+             }
+		}
+        public static List<string> AnimalsReturn
+        {
+            get { return animals; }
+        }
+    }
+}
